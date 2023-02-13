9d2b7bf76d134c044a62dc1c71eeb8a33b6dd090
dornadoff <dornadoff@gmail.com>
2023-02-13 17:53:45 +0500

    Hamm todo lar chiqishi

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 def todo(request):
+    if request.method == "POST":
+        Todo.objects.create(
+            nomi = request.POST.get("nomi"),
+            sanasi = request.POST.get("sanasi"),
+            batafsil = request.POST.get("batafsil"),
+            holat = request.POST.get("holat")
+        )
+    data = {"todo":Todo.objects.all()}
     return render(request, "todo.html")
